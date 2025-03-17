97e16b5fc1ea32c1fec9bd33385a48c573948c65
Praydos <chafik.anass2017@gmail.com>
2025-03-17 17:56:08 +0000

    upload

--- Hospital/src/main/java/org/example/hospital/web/PatientController.java
+++ Hospital/src/main/java/org/example/hospital/web/PatientController.java
@@ -5,9 +5,12 @@ import lombok.AllArgsConstructor;
 import org.example.hospital.entities.Patient;
 import org.example.hospital.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -18,9 +21,11 @@ public class PatientController {
         this.patientRepository = patientRepository;
     }
     @GetMapping("/index")
-    public String index(Model model) {
-        List<Patient> listPatients = patientRepository.findAll();
-        model.addAttribute("listPatients", listPatients);
+    public String index(Model model, @RequestParam(name = "page",defaultValue = "0") int page, @RequestParam(name = "size", defaultValue = "4") int size) {
+        Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page,size));
+        model.addAttribute("listPatients", pagePatients.getContent());
+        model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
+
         return "Patients";
     }
 }
